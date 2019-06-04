2c67ee014be944cc37e99f7f0eb812eb04adfc16
Mikkel Ridley <mikkel250@gmail.com>
2019-06-03 22:34:15 -0700

    modfy state handlers to correcty accept user input in all fields

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,18 @@ import React from 'react';
 class Profile extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = { 
+            name: '',
+            email: '',
+            address: ''
+        };
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({ [event.target.value]: event.target.value });
     }
 
     handleSubmit(event) {
@@ -22,13 +27,13 @@ class Profile extends React.Component {
             <div class="row">
             <form class="col s3"> Edit Your Profile: <br /> 
                 <label>Name:
-                <input type='text' value={this.state.value} onChange={this.handleChange} ></input><br />                 
+                <input type='text' onChange={this.handleNameChange} ></input><br />                 
                 </label>
                 <label>Email:
-                <input type='text' value={this.state.value} onChange={this.handleChange} ></input><br /> 
+                <input type='text' onChange={this.handleEmailChange} ></input><br /> 
                 </label>
                 <label>Address:
-                <input type='text' value={this.state.value} onChange={this.handleChange} ></input><br /> 
+                <input type='text' onChange={this.handleAddressChange} ></input><br /> 
                 </label>
                 <button class="btn waves-effect waves-light" type="submit" name="action">Submit<i class="material-icons right">>></i>
                 </button>
